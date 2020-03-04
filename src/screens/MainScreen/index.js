dd7c09ab2057a96015680bf0e2e25aca7ca8c290
DylanEHolland <dh1432A22291>
2020-03-04 00:17:18 -0600

    Added global css file along with Screen and BorderedBox components

--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Screen from '../../components/base/Screen/index.js';
+import BorderedBox from '../../components/base/BorderedBox/index.js';
 
 export default class MainScreen extends React.Component {
 
     render = () => {
         return (
             <>
-                I mean really?
+                <Screen>
+                    This is just a really simple message, <a href="https://google.com">Sigh</a>.
+                    <BorderedBox>
+                        This isn't tho
+                    </BorderedBox>
+                </Screen>
+                <BorderedBox>
+                    Idk about this m8;
+                </BorderedBox>
             </>
         );
     }
